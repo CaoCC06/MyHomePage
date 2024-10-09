63e38f93a0229a70609af3b0324f1e420f5caee1
CaoCC06 <2488670343@qq.com>
2024-10-09 21:59:53 +0800

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon1.ico 	new file:   public/favicon2.ico 	new file:   public/favicon3.ico 	new file:   src/App.vue 	new file:   src/assets/avatar.png 	new file:   src/main.js 	new file:   src/style.css 	new file:   src/style/App.less 	new file:   src/style/load.css 	new file:   vite.config.js

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { MotionPlugin } from '@vueuse/motion'
+import { VsButton, VsAvatar, VsTooltip, VsDialog, VsNotification, VsAlert } from 'vuesax-alpha'
+import './style.css'
+import './style/App.less'
+import App from './App.vue'
+// style
+import 'vuesax-alpha/dist/index.css'
+// vuesax darkmode
+import 'vuesax-alpha/theme-chalk/dark/css-vars.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(MotionPlugin)
+app.mount('#app')
+

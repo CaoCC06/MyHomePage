e2a54be0f3c7f8780881f56892b86411a9fc9b7b
CaoCC06 <2488670343@qq.com>
2024-10-12 15:22:24 +0800

    modified:   src/App.vue 	modified:   src/style.css 	modified:   src/style/App.less 	modified:   src/style/load.css

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,42 @@
-<script setup>
-    import Background from './template/background.vue';
-</script>
-
 <template>
     <Background/>
     <div class="main" v-motion :initial="{ opacity: 0 }" :enter="{ opacity: 1 }" :duration="2000">
-        <div class="info">
-          <div class="header">
-            <img src="./assets/avatar.png" alt="">
-          </div>
+      <div class="info">
+        <div class="header">
+          <img src="./assets/avatar.png" alt="">
+        </div>
   
-          <div class="infoText">
-            <h1>Welcome To</h1>
-            <h1><span class="qn">灿灿</span><span class="de">de小窝</span></h1>
-          </div>
+        <div class="infoText">
+          <h1>Welcome To</h1>
+          <h1><span class="qn">灿灿</span><span class="de">de小窝</span></h1>
         </div>
       </div>
+
+      <div class="typewriter">
+        <div class="content_container">
+          <p class="content_container_text">
+            Hello
+          </p>
+      
+          <ul v-for="item in personal_signature" :key="item" class="content_container_list">
+            <li class="content_container_list_item">world !</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+    <div class="footer">
+      By Cancan | ©2024
+    </div>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+    // import { ref } from 'vue';
+    import Background from './template/background.vue';
+    // personal_signature = ref([
+    //   "Hello World !",
+    //   "Hello Friend !",
+    //   "Hello Users !",
+    //   "Hello Everybody !"
+    // ])
+</script>

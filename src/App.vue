61353040c7f913720b38a9fd2a850033c9cff0a5
CaoCC06 <2488670343@qq.com>
2024-10-12 15:55:07 +0800

    modified:   index.html（删除了加载时的某些动作） 	modified:   src/App.vue 	modified:   src/main.js

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
-    <Background/>
-    <div class="main" v-motion :initial="{ opacity: 0 }" :enter="{ opacity: 1 }" :duration="2000">
-      <div class="info">
-        <div class="header">
-          <img src="./assets/avatar.png" alt="">
-        </div>
+  <Loading v-if="loading"/>
+  <Background/>
+  <div class="main" v-motion :initial="{ opacity: 0 }" :enter="{ opacity: 1 }" :duration="2000">
+    <div class="info">
+      <div class="header">
+        <img src="./assets/avatar.png" alt="">
+      </div>
   
-        <div class="infoText">
-          <h1>Welcome To</h1>
-          <h1><span class="qn">灿灿</span><span class="de">de小窝</span></h1>
-        </div>
+      <div class="infoText">
+        <h1>Welcome To</h1>
+        <h1><span class="qn">灿灿</span><span class="de">de小窝</span></h1>
       </div>
+    </div>
 
-      <div class="typewriter">
-        <div class="content_container">
-          <p class="content_container_text">
-            Hello
-          </p>
+    <div class="typewriter">
+      <div class="content_container">
+        <p class="content_container_text">
+          Hello
+        </p>
       
-          <ul v-for="item in personal_signature" :key="item" class="content_container_list">
-            <li class="content_container_list_item">world !</li>
-          </ul>
-        </div>
+        <ul v-for="item in personal_signature" :key="item" class="content_container_list">
+          <li class="content_container_list_item">world !</li>
+        </ul>
       </div>
+    </div>
 
-    </div>
-    <div class="footer">
-      By Cancan | ©2024
-    </div>
+  </div>
+  <div class="footer">
+    By Cancan | ©2024
+  </div>
 </template>
 
 <script setup>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './style.css'
 import './style/App.less'
 import App from './App.vue'
 // style
-import 'vuesax-alpha/dist/index.css'
+// import 'vuesax-alpha/dist/index.css'
 // vuesax darkmode
 import 'vuesax-alpha/theme-chalk/dark/css-vars.css'
 
